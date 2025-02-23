bb3e5e8867f5981645c0eb4358045c9c731bcfb2
champion87 <lidor071205@gmail.com>
2025-02-23 11:15:33 +0000

    template for C-python functions

---
+++ symnmf.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple
+import sys
+
+
+def c_symnmf(
+    initial_H: List[List[float]],
+    W: List[List[float]],
+    ### MORE STUFF ###
+    # k: int,
+    # max_iter: int,
+    # tol: float,
+) -> List[List[float]]:
+    pass
+
+
+def c_sym(datapoints: List[List[float]]) -> List[List[float]]:
+    pass
+
+
+def c_ddg(datapoints: List[List[float]]) -> List[List[float]]:
+    pass
+
+
+def c_norm(datapoints: List[List[float]]) -> List[List[float]]:
+    pass
+
+
+if __name__ == "__main__":
+    pass

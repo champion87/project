f9b6cbc96557b031861fe99b7ab8fb5ce66fc41a
champion87 <lidor071205@gmail.com>
2025-02-23 14:01:50 +0000

    kinda done sym (unchecked)

--- symnmf_algo.c
+++ symnmf_algo.c
@@ -2,13 +2,14 @@
 #include "matrix_utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define INVALID_DIFF (-1)
 #define IS_VALID_DIFF(diff) (diff >= 0)
 
 // Calculates the new value of that should be at a given index for the matrix H in the optimisation step.
 // The formula is Hij * (1 - beta + beta * (WHij / HHTHij))
-double calc_index(matrix_t H, matrix_t WH, matrix_t HHTH, double beta, size_t i, size_t j)
+double calc_symnmf_index(matrix_t H, matrix_t WH, matrix_t HHTH, double beta, size_t i, size_t j)
 {
     double Hij = H.data[i][j];
     double ratio = WH.data[i][j] / HHTH.data[i][j];
@@ -18,7 +19,7 @@ double calc_index(matrix_t H, matrix_t WH, matrix_t HHTH, double beta, size_t i,
 }
 
 // Performs the optimization iteration inplace, i.e. changing the value of H.data[][]
-void perform_iteration(matrix_t H, matrix_t W, double beta)
+void perform_symnmf_iteration(matrix_t H, matrix_t W, double beta)
 {
     matrix_t WH = dot(W, H);
     matrix_t HT = transpose(H);
@@ -31,7 +32,7 @@ void perform_iteration(matrix_t H, matrix_t W, double beta)
     {
         for (size_t j = 0; j < H.width; j++)
         {
-            double new_value = calc_index(H, WH, HHTH, beta, i, j);
+            double new_value = calc_symnmf_index(H, WH, HHTH, beta, i, j);
             H.data[i][j] = new_value;
         }
     }
@@ -48,7 +49,7 @@ matrix_t symnmf(matrix_t H, matrix_t W, double eps, double beta, size_t max_iter
     double diff = INVALID_DIFF;
     do
     {
-        perform_iteration(H, W, beta);
+        perform_symnmf_iteration(H, W, beta);
         diff = frobenius_distance_squared(H, old_H);
         free_matrix(old_H);
         old_H = copy_matrix(H);
@@ -57,4 +58,26 @@ matrix_t symnmf(matrix_t H, matrix_t W, double eps, double beta, size_t max_iter
 
     free_matrix(old_H);
     return H;
+}
+
+
+double calc_sym_index(matrix_t datapoints, size_t i, size_t j)
+{
+    if (i == j) { return 0; }
+    else { return exp(-0.5 * euclidean_distance_squared(datapoints.data[i], datapoints.data[j], datapoints.width)); }
+}
+
+matrix_t sym(matrix_t datapoints)
+{
+    matrix_t A = alloc_matrix(datapoints.height, datapoints.height);
+
+    for (size_t i = 0; i < datapoints.height; i++)
+    {
+        for (size_t j = 0; j < datapoints.height; j++)
+        {
+            A.data[i][j] = calc_sym_index(datapoints, i, j);
+        }
+    }
+
+    return A;
 }
--- symnmf_algo.h
+++ symnmf_algo.h
@@ -5,15 +5,15 @@
 #include "matrix_utils.h"
 
 
-
+double calc_sym_index(matrix_t datapoints, size_t i, size_t j);
 matrix_t sym(matrix_t datapoints);
 
 matrix_t ddg(matrix_t datapoints);
 
 matrix_t norm(matrix_t datapoints);
 
-void perform_iteration(matrix_t H, matrix_t W, double beta);
-double calc_index(matrix_t H, matrix_t WH, matrix_t HHTH, double beta, size_t i, size_t j);
+void perform_symnmf_iteration(matrix_t H, matrix_t W, double beta);
+double calc_symnmf_index(matrix_t H, matrix_t WH, matrix_t HHTH, double beta, size_t i, size_t j);
 matrix_t symnmf(matrix_t H, matrix_t W, double eps, double beta, size_t max_iter);
 
 

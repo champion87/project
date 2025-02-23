516bbe30de13f77ac980b23068f8c375968b20bb
avnerspr <avnerspr@gmail.com>
2025-02-23 16:14:15 +0200

    matrix utils

--- matrix_utils.c
+++ matrix_utils.c
@@ -1,15 +1,36 @@
 #include <string.h>
 #include "matrix_utils.h"
 
+matrix_t alloc_matrix(size_t height, size_t width) {
+    matrix_t result = {
+        .height = height,
+        .width = width,
+        .data = NULL,
+        .raw_data = NULL
+    };
+    result.raw_data = (double*)calloc(result.height * result.width, sizeof(*result.raw_data));
+    if (!result.raw_data) {
+        return ERR_MATRIX;
+    }
+    result.data = (double**)calloc(result.height, sizeof(*result.data));
+    if (!result.data) {
+        free(result.raw_data);
+        return ERR_MATRIX;
+    }
+    for (int i = 0; i < result.height; i++) {
+        result.data[i] = &result.raw_data[i * result.width];
+    }
+    return result;
+}
+
 void free_matrix(matrix_t mat) {
     if (mat.raw_data != NULL) {
-        mat.raw_data = NULL;    
         free(mat.raw_data);
     }
     if (mat.data != NULL) {
-        mat.data = NULL;
         free(mat.data);
     }
+    mat = ERR_MATRIX;
 }
 
 double frobenius_distance_squared(matrix_t A, matrix_t B) // TODO: check that this code is right
@@ -39,16 +60,13 @@ matrix_t dot(matrix_t A, matrix_t B) // TODO: check that this code is right
         return ERR_MATRIX;
     }
 
-    matrix_t result = {
-        .height = A.height,
-        .width = B.width,
-        .data = malloc(A.height * sizeof(double*)),
-        .raw_data = NULL
-    };
+    matrix_t result = alloc_matrix(A.height, A.width);
+    if (IS_ERR_MAT(result)) {
+        return ERR_MATRIX;
+    }
 
     for (size_t i = 0; i < A.height; i++)
     {
-        result.data[i] = malloc(B.width * sizeof(double));
         for (size_t j = 0; j < B.width; j++)
         {
             double sum = 0;
@@ -65,16 +83,13 @@ matrix_t dot(matrix_t A, matrix_t B) // TODO: check that this code is right
 
 matrix_t transpose(matrix_t A) // TODO: check that this code is right
 {
-    matrix_t result = {
-        .height = A.width,
-        .width = A.height,
-        .data = malloc(A.height * sizeof(double*)),
-        .raw_data = NULL
-    };
+    matrix_t result = alloc_matrix(A.height, A.width);
+    if (IS_ERR_MAT(result)) {
+        return ERR_MATRIX;
+    }
 
     for (size_t i = 0; i < A.width; i++)
     {
-        result.data[i] = malloc(A.height * sizeof(double));
         for (size_t j = 0; j < A.height; j++)
         {
             result.data[i][j] = A.data[j][i];
@@ -86,25 +101,10 @@ matrix_t transpose(matrix_t A) // TODO: check that this code is right
 
 matrix_t copy_matrix(matrix_t mat)
 {
-    matrix_t result = {
-        .height = mat.height,
-        .width = mat.width,
-        .data = NULL,
-        .raw_data = NULL
-    };
-    result.raw_data = (double*)calloc(result.height * result.width, sizeof(*result.raw_data));
-    if (!result.raw_data) {
-        return ERR_MATRIX;
-    }
-    result.data = (double**)calloc(result.height, sizeof(*result.data));
-    if (!result.data) {
-        free(result.raw_data);
+    matrix_t result = alloc_matrix(mat.height, mat.width);
+    if (IS_ERR_MAT(result)) {
         return ERR_MATRIX;
     }
     memcpy(result.raw_data, mat.raw_data, result.height * result.width * sizeof(*result.raw_data));
-    for (int i = 0; i < result.height; i++) {
-        result.data[i] = &result.raw_data[i * result.width];
-    }
-
     return result;
 }
